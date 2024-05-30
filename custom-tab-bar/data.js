c80a516ae53fedf977735c8006fa256375a2bf5b
XIAO Zhe <1174308963@qq.com>
2024-05-31 03:42:29 +0800

    TabBar update

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/activity/activity',
+  },
+  {
+    icon: 'chat',
+    text: '社区',
+    url: 'packages/commnunity-package/pages/community/community',
+  },
+  {
+    icon: 'user',
+    text: '个人中心',
+    url: 'pages/personal/personal',
+  },
+];
--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,42 +1,25 @@
-// custom-tab-bar/index.js
+import TabMenu from './data';
 Component({
-
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
+    active: 0,
+    list: TabMenu,
+    // value: 'label_1',
+    // list: [
+    //   { value: 'label_1', icon: 'home', ariaLabel: '首页' },
+    //   { value: 'label_2', icon: 'chat', ariaLabel: '聊天' },
+    //   { value: 'label_3', icon: 'user', ariaLabel: '我的' },
+    // ],
 
-    value: 'label_1',
-    list: [
-      { value: 'label_1', icon: 'home', ariaLabel: '首页' },
-      { value: 'label_2', icon: 'chat', ariaLabel: '聊天' },
-      { value: 'label_3', icon: 'user', ariaLabel: '我的' },
-    ],
-    // url: [
-    //   "/pages/activity/activity",
-    //   "/packages/commnunity-package/pages/community/community",
-    //   "/pages/personal/personal",
-    // ]
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-    onChange(e) {
-      this.setData({
-        value: e.detail.value,
-      });
-      console.log(e)
-      const url = "/pages/activity/activity"
-      wx.switchTab({url});
+    // onChange(e) {
+    //   this.setData({
+    //     value: e.detail.value,
+    //   });
+    //   console.log(e)
+    //   const url = "/pages/activity/activity"
+    //   wx.switchTab({url});
       // const url = "/pages/personal/personal"
       // const url1 = "/pages/activity/activity"
       // const url2 = "/packages/commnunity-package/pages/community/community";
@@ -48,8 +31,26 @@ Component({
       // }
       // if(e.detail.value=="label_2")  wx.switchTab({url2});
       // if(e.detail.value=="label_3")  wx.switchTab({url3});
+      onChange(event) {
+        this.setData({ active: event.detail.value });
+        wx.switchTab({
+          url: this.data.list[event.detail.value].url.startsWith('/')
+            ? this.data.list[event.detail.value].url
+            : `/${this.data.list[event.detail.value].url}`,
+        });
+        console.log(event.detail.value)
+      },
+      init() {
+        const page = getCurrentPages().pop();
+        const route = page ? page.route.split('?')[0] : '';
+        const active = this.data.list.findIndex(
+          (item) =>
+            (item.url.startsWith('/') ? item.url.substr(1) : item.url) ===
+            `${route}`,
+        );
+        this.setData({ active });
+      },
     },
-  }
   // methods: {
   //   onChange(e) {
   //     this.setData({
