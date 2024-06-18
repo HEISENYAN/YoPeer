f50be2984f6995027e19934f22a91cc520e39e76
XIAO Zhe <1174308963@qq.com>
2024-06-19 01:28:22 +0800

    Add popup in receive-info-edit when before navigateBack

--- packages/shop-package/pages/receive-info-edit/receive-info-edit.js
+++ packages/shop-package/pages/receive-info-edit/receive-info-edit.js
@@ -1,131 +1,18 @@
 import Toast from 'tdesign-miniprogram/toast/index';
+import {hallAddress, areaAddress} from './data'
 wx.cloud.init();
-const hallAddress = [
-  {
-    value: '100000',
-    label: '香港大学HKU',
-    children: [
-      { value: '100100', label: '校内宿舍（李国贤堂、太古堂、研究生堂）' },
-      { value: '100101', label: '一村（何东夫人堂、施德堂、利玛窦堂）' },
-      { value: '100102', label: '二村（马礼逊堂、李兆基堂、孙志新堂）' },
-      { value: '100103', label: '赛马会第三学生村' },
-      { value: '100104', label: '四村（利希慎堂、利铭泽堂、伟伦堂）' },
-      { value: '100105', label: '梅芳堂' },
-    ],
-  },
-  {
-    value: '200000',
-    label: '香港中文大学CUHK',
-    children: [
-      { value: '200100', label: '汤若望宿舍' },
-      { value: '200101', label: '伯利衡宿舍' },
-      { value: '200102', label: '陈震夏宿舍' },
-      { value: '200103', label: '恒生楼' },
-    ],
-  },
-  {
-    value: '300000',
-    label: '香港科技大学HKUST',
-    children: [
-      { value: '300100', label: '本科生宿舍一' },
-      { value: '300101', label: '本科生宿舍二' },
-      { value: '300102', label: '本科生宿舍三' },
-      { value: '300103', label: '本科生宿舍四' },
-      { value: '300104', label: '本科生宿舍五' },
-      { value: '300105', label: '本科生宿舍六' },
-      { value: '300106', label: '本科生宿舍七' },
-      { value: '300107', label: '本科生宿舍八' },
-      { value: '300108', label: '本科生宿舍九' },
-      { value: '300109', label: '赛马会宿舍' },
-      { value: '300110', label: 'UA' },
-      { value: '300111', label: 'SKCC' },
-      { value: '300112', label: 'GGT' },
-    ],
-  },
-  {
-    value: '400000',
-    label: '香港理工大学PolyU',
-    children: [
-      { value: '400100', label: '何文田宿舍(佛光街15号)' },
-      { value: '400101', label: '红磡宿舍(红荔道1号)' },
-    ],
-  },
-  {
-    value: '500000',
-    label: '香港城市大学CityU',
-    children: [
-      { value: '500100', label: '九龙塘宿舍' },
-      { value: '500101', label: '马鞍山宿舍' },
-    ],
-  },
-  {
-    value: '600000',
-    label: '香港浸会大学BU',
-    children: [
-      { value: '600100', label: '浸会大学宿舍' },
-    ],
-  },
-  {
-    value: '700000',
-    label: '香港岭南大学LNU',
-    children: [
-      { value: '700100', label: '南部宿舍' },
-      { value: '700101', label: '赛马会宿舍' },
-      { value: '700102', label: '黄虎泉堂和吴洁仪堂' },
-    ],
-  },
-  {
-    value: '800000',
-    label: '香港教育大学EduHK',
-    children: [
-      { value: '800100', label: '罗富国堂' },
-      { value: '800101', label: '葛亮洪堂' },
-      { value: '800102', label: '柏立基堂' },
-      { value: '800103', label: '赛马会学生宿舍' },
-    ],
-  }
-];
-const areaAddress = [
-  {
-    value: '900100',
-    label: '九龙',
-    children: [
-      { value: '900101', label: '观塘区' },
-      { value: '900102', label: '黄大仙区' },
-      { value: '900103', label: '九龙城区' },
-      { value: '900104', label: '深水埗区' },
-      { value: '900105', label: '油尖旺区' },
-    ],
-  },
-  {
-    value: '910000',
-    label: '香港岛',
-    children: [
-      { value: '910100', label: '东区' },
-      { value: '910200', label: '南区' },
-      { value: '910300', label: '湾仔区' },
-      { value: '910400', label: '中西区' },
-    ],
-  },
-  {
-    value: '920000',
-    label: '新界',
-    children: [
-      { value: '920100', label: '北区' },
-      { value: '920200', label: '大埔区' },
-      { value: '920300', label: '葵青区' },
-      { value: '920400', label: '离岛区' },
-      { value: '920500', label: '荃湾区' },
-      { value: '920600', label: '沙田区' },
-      { value: '920700', label: '屯门区' },
-      { value: '920800', label: '西贡区' },
-      { value: '920900', label: '元朗区' },
-    ],
-  }
-]
 var app = getApp()
 Page({ 
   data: {
+    // 点击返回时
+
+    // showMultiBtn: false,
+    // multiBtnList: [
+    //   { content: '次要按钮', theme: 'light' },
+    //   { content: '次要按钮', theme: 'light' },
+    //   { content: '主要按钮', theme: 'primary' },
+    // ],
+    delta: 0,  //后退层数
     consigneeName: '',
     consigneeNameValue: '',  //表单显示
     consigneePhoneNum: '',
@@ -158,6 +45,25 @@ Page({
     receiverInfo:{},
     phoneInput: [],
   },
+
+  // 返回时弹窗 //
+  showDialog(e) {  //点击返回时显示弹窗
+    this.setData({
+      visible: true,
+    })
+  },
+  onVisibleChange(e) {  //点击空白处取消弹窗
+    this.setData({
+      visible: e.detail.visible,
+    });
+  },
+  // closeDialog() {  //
+  //   console.log("close dialog")
+  //   this.setData({
+  //     visible: false,
+  //   })
+  // },
+
   onShow() {
     this.setData({
       consigneeName: app.globalData.consigneeName,
@@ -178,16 +84,14 @@ Page({
     //   this.setData({ maxphonenum: 9, phoneInput: [formattedValue] });
     // }
   },
+  noSave(){  //返回不保存
+    wx.navigateBack()
+  },
+  save(){  //返回并保存
+    wx.navigateBack()
 
-  handleBack(e){  //点击返回时触发
-    wx.showToast({
-      title: "确定返回吗",
-      icon:"error",
-      duration:2000
-    })
-    // console.log(e)
     // if(!this.checkNotNull(this.data.consigneeName))  console.log("null 1")
-    console.log(this.data.selectedAreaAddress)
+    // console.log(this.data.selectedAreaAddress)
     // if(this.data.selectedAreaAddress.length==0)  console.log("null 1")
   },
   checkNotNull(params) {  //检查非空字符，非空：返回true
@@ -208,6 +112,7 @@ Page({
     else return false
   },
   saveAddress(e){
+    console.log(e)
     if(this.checkSubmit(e)&&this.checkPhoneNum(e)){
       var ifHallResident = 0
       var address = ''
@@ -304,13 +209,18 @@ Page({
     else if (e.detail.value[0]=="+86")  this.setData({maxphonenum: 11})
   },
   onSelectAddress(e){  //宿舍住户，校外住户切换
+    
+    if(e.target.dataset.selectedAddress!=this.data.selectedAddressType) {  //校内校外切换时，清空选择
+      this.setData({
+        selectedHallAddress: [],
+        selectedAreaAddress: [],
+        hallSelectNote1:'请选择宿舍',
+        areaSelectNote: '请选择地区',
+      });
+    }
     this.setData({
       selectedAddressType:e.target.dataset.selectedAddress,
-      selectedHallAddress: [],
-      hallSelectNote1:'请选择宿舍',
-      selectedAreaAddress: [],
-      areaSelectNote: '请选择地区',
-    })
+    });
   },
   onOpenCascader(){  //级联选择器展示（宿舍住户，校外住户共用）
     this.setData({
